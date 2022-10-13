a858574251421a4f1ef62848151c76e3936568b2
G6D <41470551+g6dd@users.noreply.github.com>
2022-10-13 20:14:14 +0200

    adding more Math functions

--- README.md
+++ README.md
@@ -27,18 +27,27 @@ const math = require("./sumMath");
 math.abs(-2)                          // 2
 math.acos(-1)                         // 3.141592653589793
 math.avg(0,1,2,3,4)                   // 2
+math.cbrt(34)                         // 3.239611801277483
 math.ceil(Math.PI)                    // 4
+math.clz32(12345)                     // 18
 math.coll(128)                        // [128, 64, 32, 16, 8, 4, 2, 1]
-math.exec("(12*(24+2))/(24/(12+2))")  // 182
+math.exec("(12*(24+2))/(24/(12+2%))")  // 182
 math.exp(1)                           // 2.7182818284590455
+math.expm1(1)                         // 1.7182818284590455
 math.floor(Math.PI)                   // 3
 math.frct(6)                          // 720
+math.hypot(1,2,3,4)                   // 5.477225575051661
 math.max(0,1,2,-3)                    // 2
 math.med(0,1,2,3,4)                   // 2
 math.min(0,1,2,-3)                    // -3
 math.pow(2,3)                         // 8
 math.round(Math.PI,3)                 // 3.142
 math.rt(8,4)                          // 1.6817928305074292
+math.sign(-15.6)                      // -1
 math.sqrt(8)                          // 3
 math.trunc(Math.PI)                   // 3
+
+math.E                                // 2.7182818284590455
+math.SQRT1_2                          // 0.7071067811865476
+math.SQRT2                            // 1.4142135623730951
 ```
--- sumMath.js
+++ sumMath.js
@@ -6,89 +6,35 @@
 const math = {};
 
 /**
- * Returns the largest of the numbers in an Array
- * @param  {...number} n Number Array
- */
-math.min = (...n) => Number(n.filter(e=>!isNaN(Number(e))).sort((a,b)=>a-b)[0]);
-/**
- * Returns the largest of the numbers in an Array
- * @param  {...number} n Number Array
- */
-math.max = (...n) => Number(n.filter(e=>!isNaN(Number(e))).sort((a,b)=>a-b).reverse()[0]);
-/**
  * Returns the absolute value of a number
  * @param  {number} n Number
  */
 math.abs = (n) => Number(n>=0?n:-n);
-/**
- * Returns the value of a number rounded to the nearest integer
- * @param  {number} n Number
- * @param  {number} dec Number of shown decimals
- */
-math.round = (n,dec=2) => Number(Number(n).toFixed(dec));
-/**
- * Returns the value of a base raised to a power
- * @param  {number} n Base Number
- * @param  {number} exp Exponent
- */
-math.pow = (n,exp=2) => Number(n)**Number(exp);
-/**
- * Returns the root of a number
- * @param  {number} n Base Number
- * @param  {number} exp Exponent
- */
-math.rt = (n,exp) => Number((isNaN(n) || !n)?NaN:math.pow(n,(1/exp)));
-/**
- * Returns the square root of a number
- * @param  {number} n Base Number
- */
-math.sqrt = (n) => math.rt(n,2);
 /**
  * Returns the inverse cosine of a number
  * @param  {number} n Number
  */
 math.acos = (n) => (isNaN(n)||n>=1||n<=-2)?(n==1?0:NaN):((n=2+(Number(n)*2)),Number(!!n?Math.PI/(n):Math.PI));
 /**
- * Returns the factorial product of all integers between 1 and the given number
- * @param  {number} n Number
- */
-math.frct = (n) => Number((isNaN(n)||n<=1)?n:n*math.frct(n-1));
-/**
- * Returns {@link Math.E} raised to the power of a number
- * 
- * and
- * 
- * yes i know `Math.E**n` or `Math.pow(Math.E,n)`..
- * 
- * but that would be too easy..
- * @param  {number} n Number
- */
-math.exp = (n) => Number((isNaN(n)||n==0)?NaN:(1+Array.from({length:100},(e,i)=>((n**i)/math.frct(i))).filter(e=>e!=Infinity).reduce((a,b)=>a+=b)));
-/**
  * Returns the average from an Array
  * @param  {...number} n Number
  */
 math.avg = (...n) => !(n.length)?0:Number((n.map(Number).reduce((a,b)=>a+=b))/n.length);
 /**
- * Returns the median from an Array
- * @param  {...number} n Number
- */
-math.med = (...n) => Number(((n=n.sort((a,b)=>a-b).map(Number).splice(n.length/2-(n.length%2==0?1:0),n.length%2==0?2:1)),(n.length%2==0?n.reduce((a,b)=>a+=b)/2:n[0])));
+* Returns the cube root of a number
+* @param  {number} n Base Number
+*/
+math.cbrt = (n) => math.rt(n,3);
 /**
- * Returns the integer part of a number by removing decimals
- * @param  {number} n Number
- */
-math.trunc = (n) => Number(n.toString().split(".")[0]);
-/**
- * Always rounds down and returns the largest integer less than or equal to a given number
- * @param  {number} n Number
- */
-math.floor = (n) => Number(math.trunc(n)-(n<0?(/[\.]/g.test(n)?1:0):0));
-/**
- * Always rounds up and returns the smallest integer greater than or equal to a given number
+ * Returns the smallest integer greater than or equal to a given number
  * @param  {number} n Number
  */
 math.ceil = (n) => Number(-math.floor(-n));
+/**
+ * Returns the number of leading zero bits in the 32-bit binary representation of a number
+ * @param {number} n Number
+ */
+math.clz32 = (n) => (!(n>>>=0))?32:(Math.abs(n)).toString(2).padStart(32,"0").split("").map(e=>Math.sign(n)<0?+(!Number(e)):e).join("").split("1")[0].length;
 /**
  * Returns an generated Array from `n` to 1 in the Collatz conjecture/problem
  * @param  {number} n Number
@@ -103,9 +49,102 @@ math.coll = (n) => {
  * @param  {string} str simple equation (no spaces allowed)
  */
 math.exec = (str) => {
-    if(/[^\d*+-/.%\(\)]/g.test(str)) return -1;
-    if(/\([\S\s]+\)/g.test(str))str=math.exec(str.replace(/\([\d*+-/.%]+\)/g,e=>math.exec(e.substr(1,e.indexOf(")")-1))+e.substr(e.indexOf(")")+1)));
-    return Number(eval(str.replace?str.replace(/[\d\.]+\%/g,e => (e.substr(0,e.length-1)/100)):str));
+    if(/[^\d*+-/.%\(\)]/g.test(str)) return NaN;
+    const eq=str=>str.toString().replace(/[\(\)]/g,"").replace(/(\d+(.\d+)?%?)[*-/+](\d+(.\d+)?%?)/g,e=>(s=>e.split(s).map(Number).reduce((a,b)=>{switch(s){case"+":return a+=b; case"-":return a-=b;case"*":return a*=b;case"/":return a/=b;}}))(e.split(/[\d.]/g).join("")[0]));
+    if(/\([\d*+-/.%]+\)/g.test(str))str=math.exec(str.replace(/\([\d*+-/.%]+\)/g,e=>e.substr(1,e.length-2).replace(/(\d+(.\d+)?%?)[*-/+](\d+(.\d+)?%?)/g,eq)));
+    return Number(/[*-/+]/g.test(str)?eq(str):str);
 }
+/**
+ * Returns {@link Math.E} raised to the power of a number
+ * 
+ * and
+ * 
+ * yes i know `Math.E**n` or `Math.pow(Math.E,n)`..
+ * 
+ * but that would be too easy..
+ * @param  {number} n Number
+ */
+math.exp = (n) => Number((isNaN(n)||n==0)?NaN:(1+Array.from({length:100},(e,i)=>((n**i)/math.frct(i))).filter(e=>e!=Infinity).reduce((a,b)=>a+=b)));
+/**
+ * Returns e raised to the power of a number, subtracted by 1
+ * @param {*} n 
+ * @returns 
+ */
+math.expm1 = (n) => Number(math.exp(n)-1);
+/**
+ * Returns the largest integer less than or equal to a given number
+ * @param  {number} n Number
+ */
+math.floor = (n) => Number(math.trunc(n)-(n<0?(/[\.]/g.test(n)?1:0):0));
+/**
+ * Returns the factorial product of all integers between 1 and the given number
+ * @param  {number} n Number
+ */
+math.frct = (n) => Number((isNaN(n)||n<=1)?n:n*math.frct(n-1));
+/**
+ * Returns the square root of the sum of squares of its arguments
+ * @param  {...number} n Number Array
+ */
+math.hypot = (...n) => math.sqrt(n.map(n=>n**2).reduce((a,b)=>a+=b));
+/**
+ * Returns the largest of the numbers in an Array
+ * @param  {...number} n Number Array
+ */
+math.max = (...n) => Number(n.filter(e=>!isNaN(Number(e))).sort((a,b)=>a-b).reverse()[0]);
+/**
+ * Returns the median from an Array
+ * @param  {...number} n Number
+ */
+math.med = (...n) => Number(((n=n.sort((a,b)=>a-b).map(Number).splice(n.length/2-(n.length%2==0?1:0),n.length%2==0?2:1)),(n.length%2==0?n.reduce((a,b)=>a+=b)/2:n[0])));
+/**
+ * Returns the smallest of the numbers in an Array
+ * @param  {...number} n Number Array
+ */
+math.min = (...n) => Number(n.filter(e=>!isNaN(Number(e))).sort((a,b)=>a-b)[0]);
+/**
+ * Returns the value of a base raised to a power
+ * @param  {number} n Base Number
+ * @param  {number} exp Exponent
+ */
+math.pow = (n,exp=2) => Number(n)**Number(exp);
+/**
+ * Returns the value of a number rounded to the nearest integer
+ * @param  {number} n Number
+ * @param  {number} dec Number of shown decimals
+ */
+math.round = (n,dec=2) => Number(Number(n).toFixed(dec));
+/**
+ * Returns the root of a number
+ * @param  {number} n Base Number
+ * @param  {number} exp Exponent
+ */
+math.rt = (n,exp) => Number((isNaN(n) || !n)?NaN:math.pow(n,(1/exp)));
+/**
+ * eturns 1 or -1, indicating the sign of the number passed
+ * @param {number} n Number
+ */
+math.sign = (n) => isNaN(n)?NaN:(n==math.abs(n)?1:-1);
+/**
+ * Returns the square root of a number
+ * @param  {number} n Base Number
+ */
+math.sqrt = (n) => math.rt(n,2);
+/**
+ * Returns the integer part of a number by removing decimals
+ * @param  {number} n Number
+ */
+math.trunc = (n) => Number(n.toString().split(".")[0]);
+/**
+ * Represents Euler's number
+ */
+math.E = math.exp(1);
+/**
+ * Represents the square root of 2
+ */
+math.SQRT2 = math.sqrt(2);
+/**
+ * Represents the square root of 1/2
+ */
+math.SQRT1_2 = math.sqrt(1/2);
 
 module.exports = math;
